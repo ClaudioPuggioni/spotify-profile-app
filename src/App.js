6eff4cf517905d0fc4308d14fdff387e049c15e6
Mohamed Ibrahim Fathy <eng.mohammedibrahim904@gmail.com>
2022-07-29 10:53:44 +0200

    Initiated the project and created the app label

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import './App.css'
+import AppLabel, { TYPES } from './components/atoms/AppLabel/AppLabel';
 function App() {
   return (
-    <div>
-      <h1>Spotify</h1>
+    <div className='App'>
+      <AppLabel type={TYPES.PAR} isBold>Hello Claudio</AppLabel>
     </div>
   );
 }
