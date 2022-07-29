75ee50e4f93292ac57a20456279f8a1790de263f
Mohamed Ibrahim Fathy <eng.mohammedibrahim904@gmail.com>
2022-07-29 10:56:35 +0200

    Initial State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css'
-import AppLabel, { TYPES } from './components/atoms/AppLabel/AppLabel';
 function App() {
   return (
     <div className='App'>
-      <AppLabel type={TYPES.PAR} isBold>Hello Claudio</AppLabel>
+      
     </div>
   );
 }
