fbb56778886645c7e0dc17a0262e629a3c599fa3
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-29 03:37:03 -0700

    done with roundButtons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css'
-import AppLabel, { TYPES } from './components/atoms/AppLabel/AppLabel';
+import "./App.css";
+import AppLabel, { TYPES } from "./components/atoms/AppLabel/AppLabel";
+import RoundedButton from "./components/atoms/RoundedButton/RoundedButton";
 function App() {
+  function api() {}
+
   return (
-    <div className='App'>
-      <AppLabel type={TYPES.PAR} isBold>Hello Claudio</AppLabel>
+    <div className="App">
+      {/* <AppLabel type={TYPES.PAR} isBold>Hello Claudio</AppLabel> */}
+      {/* <RoundedButton>Log in to spotify</RoundedButton> */}
+      {/* <RoundedButton isInline>Logout</RoundedButton> */}
+      {/* <RoundedButton isInline>See more</RoundedButton> */}
     </div>
   );
 }
