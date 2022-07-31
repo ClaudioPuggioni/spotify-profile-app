f8945efb77bd056e6382b7a76cfe065cc75067f2
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-31 08:57:24 -0700

    completed recent template

--- src/components/templates/Recent/Recent.jsx
+++ src/components/templates/Recent/Recent.jsx
@@ -4,12 +4,21 @@ import "./styles.css";
 // import { TYPES as LABELTYPES } from "../../atoms/AppLabel/AppLabel";
 // import { SPOTIFYWHITE } from "../../../styles/colors";
 
+const tracksArr = [
+  { imageNameExt: "logicBomb-unlimited.png", trackName: "Neighbour Of The Beast", artistName: "Logic Bomb", albumName: "Unlimited", songLength: "7:13" },
+  { imageNameExt: "macAyres-driveSlow.jpg", trackName: "Easy", artistName: "Mac Ayres", albumName: "Drive Slow", songLength: "5:14" },
+  { imageNameExt: "fridayNightPlans-plasticLove.jpg", trackName: "Plastic Love", artistName: "Friday Night Plans", albumName: "Plastic Love", songLength: "4:36" },
+];
+
 export default function Recent() {
   return (
     <div id="recentContainer">
-      <AppLabel type={TYPES.SUB_TITLE} style isBold>
-        Recently Played Tracks
-      </AppLabel>
+      <div id="recentDiv">
+        <AppLabel type={TYPES.SUB_SUB_TITLE} style={{ color: SPOTIFYWHITE }} isBold>
+          Recently Played Tracks
+        </AppLabel>
+        <TracksList tracksArr={tracksArr} />
+      </div>
     </div>
   );
 }
--- src/components/templates/Test/Test.jsx
+++ src/components/templates/Test/Test.jsx
@@ -1,9 +1,23 @@
 import "./styles.css";
+import AppLabel, { TYPES } from "../../atoms/AppLabel/AppLabel";
+import { SPOTIFYWHITE } from "../../../styles/colors";
+import TracksList from "../../molecules/TracksList/TracksList";
+
+const tracksArr = [
+  { imageNameExt: "logicBomb-unlimited.png", trackName: "Neighbour Of The Beast", artistName: "Logic Bomb", albumName: "Unlimited", songLength: "7:13" },
+  { imageNameExt: "macAyres-driveSlow.jpg", trackName: "Easy", artistName: "Mac Ayres", albumName: "Drive Slow", songLength: "5:14" },
+  { imageNameExt: "fridayNightPlans-plasticLove.jpg", trackName: "Plastic Love", artistName: "Friday Night Plans", albumName: "Plastic Love", songLength: "4:36" },
+];
 
 export default function Test() {
   return (
     <div id="testContainer">
-      <></>
+      <div id="recentDiv">
+        <AppLabel type={TYPES.SUB_SUB_TITLE} style={{ color: SPOTIFYWHITE }} isBold>
+          Recently Played Tracks
+        </AppLabel>
+        <TracksList tracksArr={tracksArr} />
+      </div>
     </div>
   );
 }
