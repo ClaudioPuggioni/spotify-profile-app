f9ac5fa0256ecdc05465fa987321145e537f233a
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-31 11:59:19 -0700

    completed all pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
+import ReactDOM from "react-dom/client";
 import store from "./store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserInterface from "./UserInterface";
