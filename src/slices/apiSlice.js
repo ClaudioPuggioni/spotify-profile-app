842499210775603da17442e0fe8119f81d9b5218
Claudio Puggioni <claudieddu@hotmail.com>
2022-08-01 21:44:48 -0700

    got token

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -4,14 +4,12 @@ import { Buffer } from "buffer";
 
 const dev_id = "6d9d2ada4edc4edbbaf9123d39707310";
 const client_secret = "25c9a61a43954e85a98ca6613c4dedae";
+const redirect_uri = "http://localhost:3000/";
 
 const getAuth = () => {
-  const dev_id = "6d9d2ada4edc4edbbaf9123d39707310";
-  const redirect_uri = "http://localhost:3000/";
+  console.log("ping");
   const state = nanoid();
   const scope = "playlist-read-private user-top-read user-read-recently-played";
-  const client_secret = "25c9a61a43954e85a98ca6613c4dedae";
-  console.log("ping");
   return `https://accounts.spotify.com/authorize?client_id=${dev_id}&response_type=code&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}`;
 };
 
@@ -50,17 +48,14 @@ const apiSlice = createSlice({
 const testAuth = createAsyncThunk("apiRedux/testAuth", async (code) => {
   let response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
+    body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&client_id=${dev_id}&client_secret=${client_secret}`,
     headers: {
-      Authorization: "Basic " + Buffer.from(dev_id + ":" + client_secret).toString("base64"),
+      Authorization: `Basic ${Buffer.from(dev_id + ":" + client_secret).toString("base64")}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: {
-      grant_type: "authorization code",
-      code: "",
-      redirectui: "http://localhost:3000/profile",
-    },
   });
-  console.log("Finished Testing!");
+  let data = await response.json();
+  console.log("Finished Testing!", data);
   return [response.status, code];
 });
 
